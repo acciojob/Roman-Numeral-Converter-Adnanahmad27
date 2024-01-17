b3a14029a0282fd7eaafe3f712929b03d5e27deb
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-01-17 13:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,17 +24,17 @@ function convertToRoman(num) {
 		}
 	}
 	let romanNo = "";
-	while(num < 0){
+	while(num > 0){
 		let arr = findRomanNo(num);
 		romanNo += arr[0];
 		num -= arr[1];
 	}
-	
+	return romanNo;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
- console.log(convertToRoman(36));
+// console.log(convertToRoman(36));
 
 
 
